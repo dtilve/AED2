095ad395d6157aceb359d211e385cd2a0a2b52be
NataliaP1 <NataliaP1@users.noreply.github.com>
2015-11-25 19:31:20 -0300

    Update Driver.h

--- Driver.h
+++ Driver.h
@@ -12,21 +12,21 @@ namespace aed2 {
 
 class Driver
 {
-    public:
+	public:
 
-        Driver();
-        ~Driver();
+	        Driver();
+	        ~Driver();
 
-        /// Generadores del Campus
+        	/// Generadores del Campus
 		void crearCampus(Nat ancho, Nat alto);
 		void agregarObstaculo(Posicion p);
 
-        /// Observadores del Campus
+        	/// Observadores del Campus
 		Nat filas() const;
 		Nat columnas() const;
 		bool ocupada(Posicion p) const;
 
-        /// Generadores de CampusSeguro
+        	/// Generadores de CampusSeguro
 		void comenzarRastrillaje(const Dicc<Agente,Posicion>& d);
 		void ingresarEstudiante(Nombre n, Posicion p);
 		void ingresarHippie(Nombre n, Posicion p); 
@@ -34,7 +34,7 @@ class Driver
 		void moverHippie(Nombre n);
 		void moverAgente(Agente pl);
 		
-        /// Observadores de CampusSeguro
+        	/// Observadores de CampusSeguro
 		Nombre iesimoEstudiante(Nat i) const;
 		Nombre iesimoHippie(Nat i) const;
 		Nat iesimoAgente(Nat i) const;
@@ -65,6 +65,107 @@ class Driver
 
 }; // class Driver
 
+Driver(){
+	c=Campus();
+	cs=CampusSeguro();
+}
+~Driver(){
+	c.~Campus();
+	cs.~CampusSeguro();
+}
+void crearCampus(Nat ancho, Nat alto){
+	c=Campus(ancho,alto);
+}
+void agregarObstaculo(Posicion p){
+	c.AgregarObstaculo(p);
+}
+Nat filas() const{
+	return c.Filas();
+}
+Nat columnas() const{
+	return c.Columnas();
+}
+bool ocupada(Posicion p) const{
+	return c.EstaOcupada(p);
+}
+void comenzarRastrillaje(const Dicc<Agente,Posicion>& d){
+	cs.ComenzarRastrillaje(d);
+}
+void ingresarEstudiante(Nombre n, Posicion p){
+	cs.IngresarEstudiante(n,p);
+}
+void ingresarHippie(Nombre n, Posicion p){
+	cs.IngresarHippie(n,p);
+}
+void moverEstudiante(Nombre n, Direccion d){
+	cs.MoverEstudiante(n,d);
+}
+void moverHippie(Nombre n){
+	cs.MoverHippie(n);		
+}
+void moverAgente(Agente pl){
+	cs.MoverAgente(pl);
+}
+Nombre iesimoEstudiante(Nat i) const{
+	Iterador<Conj<Nombre>> it = cs.Estudiantes();
+	int j=0;
+	while((j<i) && (it.HaySiguiente())){
+		it.Avanzar();
+	}
+	return it.Siguiente();
+}
+Nombre iesimoHippie(Nat i) const{
+	Iterador<Conj<Nombre>> it = cs.Hippies();
+	int j=0;
+	while((j<i) && (it.HaySiguiente())){
+		it.Avanzar();
+	}
+	return it.Siguiente();
+}
+Nat iesimoAgente(Nat i) const{
+	Iterador<Conj<Placa>> it = cs.Agentes();
+	int j=0;
+	while((j<i) && (it.HaySiguiente())){
+		it.Avanzar();
+	}
+	return it.Siguiente();
+}
+  Nat cantEstudiantes() const{
+  	return cs.CantEstudiantes();
+  }
+ Nat cantHippies() const{
+ 	return cs.CantHippies();
+ }
+ Nat cantAgentes() const{
+	Iterador<Conj<Placa>> it = cs.Agentes();
+	int i=0;
+	while(it.HaySiguiente()){
+		it.Avanzar();
+		i++;
+	}
+	return i;
+ }
+Posicion posEstudianteYHippie(Nombre n) const{
+	return cs.PosicionEstudianteYHippie(n);
+}
+Posicion posAgente(Agente pl) const{
+	return cs.PosAgente(pl);
+}
+Nat cantSanciones(Agente pl) const{
+	return cs.CantSanciones(pl);
+}
+Nat cantHippiesAtrapados(Agente pl) const{
+	return cs.CantHippiesAtrapados(pl);
+}
+Agente masVigilante() const{
+	return cs.MasVigilante();
+}
+const Conj<Agente> conMismasSanciones(Agente a) const{
+	return cs.ConMismasSanciones(a);
+}
+const Conj<Agente> conKSanciones(Nat k){
+	return cs.ConKSanciones(k);
+}
 
 } // namespace aed2
 
